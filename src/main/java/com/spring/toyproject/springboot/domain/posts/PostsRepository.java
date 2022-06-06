3528402a205d247861046d50abd54f16c651383f
mina <alsk464@gmail.com>
2022-06-06 14:59:07 +0900

    템플릿 엔진 머스테치(mustache) 연동 및 화면 구성

--- src/main/java/com/spring/toyproject/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/spring/toyproject/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.spring.toyproject.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Posts 클래스로 Database를 접근하게 해줄 JpaRepository 인터페이스
@@ -8,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/spring/toyproject/springboot/web/PostsApiController.java
+++ src/main/java/com/spring/toyproject/springboot/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.spring.toyproject.springboot.web.dto.PostsResponseDto;
 import com.spring.toyproject.springboot.web.dto.PostsSaveRequestDto;
 import com.spring.toyproject.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,22 +14,28 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    // 등록 api
+    // 게시글 등록 api
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
-    // 수정 api
+    // 게시글 수정 api
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    // 조회 api
+    // 게시글 단건 조회 api
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
+    // 게시글 삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
