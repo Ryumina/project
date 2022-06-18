6595de6d9b39783bd48db3bf32b6e2ee95e5ea19
mina <alsk464@gmail.com>
2022-06-18 11:58:36 +0900

    스프링 시큐리티 설정 복습

--- src/main/java/com/spring/toyproject/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/spring/toyproject/springboot/config/auth/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.toyproject.springboot.config.auth;
 
 import com.spring.toyproject.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,7 +13,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
-    protected void configure() throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
+        // 스프링 시큐리티에서 csrf protection은 default 이다.
+        // 보안 수준을 향상시키는 csrf 를 왜 disable 할까?
+        // -> non-browser clients 만을 위한 서비스라면 csrf를 disable 하여도 좋다.
+        // -> rest api를 사용하는 서버는 session 기반 인증과 달리 서버에 인증 정보를 보관하지 않기 때문에 disable 해도 좋다.
         http.csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
