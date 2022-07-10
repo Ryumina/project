ce812e14865485bf8661ddaf82d55cf4404084c7
mina <alsk464@gmail.com>
2022-07-10 12:20:09 +0900

    네이버 로그인 구현 수정 및 로그인 체크 추가

--- src/main/resources/static/js/app/board/findListBoard.js
+++ src/main/resources/static/js/app/board/findListBoard.js
@@ -1,7 +1,23 @@
 var findListBoard = {
 
     init: function () {
-        console.log('### findListBoard');
+        var _this = this;
+
+        // 글등록 버튼 클릭시
+        $("#createBoardBtn").on('click', function () {
+            _this.loginCheck();
+        });
+    },
+    // 로그인 체크
+    loginCheck: function () {
+        var loginId = $("#user").text();
+
+        if(loginId == '' || loginId == null) {
+            alert('로그인이 필요합니다.');
+            return;
+        } else {
+            window.location.href = '/createBoard';
+        }
     }
 }
 
